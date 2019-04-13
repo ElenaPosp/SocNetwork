54e1d46e8e381fd50056e40a1d3fe526458d48df
ElenaPosp <pospelova_elena_al@mail.ru>
2019-04-14 02:15:43 +0500

    Add cell for fees

--- Course2FinalTask/Course2FinalTask/Code/FeedViewController.swift
+++ Course2FinalTask/Course2FinalTask/Code/FeedViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import DataProvider
 
 class FeedViewController: UIViewController  {
 
+    let posts = DataProviders.shared.postsDataProvider.feed()
+    let cellIdentifier = String(describing: FeedTableViewCell.self)
+    
     lazy var feedTableView: UITableView = {
         let table = UITableView(frame: self.view.frame)
         return table
@@ -18,7 +22,9 @@ class FeedViewController: UIViewController  {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTableView()
-
+        
+        let nib = UINib(nibName: cellIdentifier, bundle: nil)
+        feedTableView.register(nib, forCellReuseIdentifier: cellIdentifier)
         
     }
 
@@ -35,10 +41,21 @@ extension FeedViewController: UITableViewDelegate {
 
 extension FeedViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let post = posts[indexPath.row]
+        let cell = feedTableView.dequeueReusableCell(withIdentifier: cellIdentifier,
+                                                     for: indexPath) as! FeedTableViewCell
+        
+        cell.authorAvatarImageView.image = post.authorAvatar
+        cell.authorNameLabel.text = post.authorUsername
+        cell.postImageView.image = post.image
+        cell.postTimeLabel.text = post.createdTime.description
+        cell.likesLabel.text = "Likes: \(post.likedByCount)"
+        cell.postDescriptionLabel.text = post.description
+        cell.likeImageView.tintColor = .lightGray
+        return cell
     }
 }
---
+++ Course2FinalTask/Course2FinalTask/Views/FeedTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  FeedTableViewCell.swift
+//  Course2FinalTask
+//
+//  Created by Елена Поспелова on 14/04/2019.
+//  Copyright © 2019 e-Legion. All rights reserved.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var authorAvatarImageView: UIImageView!
+    @IBOutlet weak var authorNameLabel: UILabel!
+    
+    @IBOutlet weak var postTimeLabel: UILabel!
+    @IBOutlet weak var postImageView: UIImageView!
+    
+    @IBOutlet weak var likesLabel: UILabel!
+    @IBOutlet weak var likeImageView: UIImageView!
+    @IBOutlet weak var postDescriptionLabel: UILabel!
+    
+    
+
+}
