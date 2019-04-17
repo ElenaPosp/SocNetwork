25cfb3b34670b4feada15893a6dcf9f87c261185
ElenaPosp <pospelova_elena_al@mail.ru>
2019-04-18 01:44:16 +0500

    Add recognizers and cells for collection view

---
+++ Course2FinalTask/Course2FinalTask/Views/ProfileCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileCollectionViewCell.swift
+//  Course2FinalTask
+//
+//  Created by Елена Поспелова on 15/04/2019.
+//  Copyright © 2019 e-Legion. All rights reserved.
+//
+
+import UIKit
+
+class ProfileCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var image: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Course2FinalTask/Course2FinalTask/Views/ProfileFirstCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileFirstCell.swift
+//  Course2FinalTask
+//
+//  Created by Елена Поспелова on 15/04/2019.
+//  Copyright © 2019 e-Legion. All rights reserved.
+//
+
+import UIKit
+
+class ProfileFirstCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
