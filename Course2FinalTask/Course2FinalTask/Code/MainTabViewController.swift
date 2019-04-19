445b50c0904cc763f4daa7ab1e6ea6ee917c3f3f
ElenaPosp <pospelova_elena_al@mail.ru>
2019-04-20 02:06:38 +0500

    Add titles

--- Course2FinalTask/Course2FinalTask/Code/MainTabViewController.swift
+++ Course2FinalTask/Course2FinalTask/Code/MainTabViewController.swift
@@ -11,14 +11,25 @@ import UIKit
 
 class MainTabViewController: UITabBarController, UITabBarControllerDelegate {
     override func viewDidLoad() {
-        super.viewDidLoad()        
+        super.viewDidLoad()
         setup()
     }
     
     func setup() {
-        
         let feedVC = FeedNavigationViewController()
         let profileVC = ProfileNavigationViewController()
+      
+        let profileTitle = UILabel(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 40))
+        profileTitle.textAlignment = .center
+        profileTitle.text = "Profile"
+        profileVC.navigationBar.addSubview(profileTitle)
+        
+            
+        let feddTitle = UILabel(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 40))
+        feddTitle.textAlignment = .center
+        feddTitle.text = "Feed"
+        feedVC.navigationBar.addSubview(feddTitle)
+            
         
         feedVC.tabBarItem = UITabBarItem(title: "Feed", image: UIImage(named: "feed"), tag: 1)
         profileVC.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(named: "profile"), tag: 2)
