9887a108aa3cc8ae8d18fd6a5f762f79c530a1a6
ElenaPosp <pospelova_elena_al@mail.ru>
2019-07-04 00:24:06 +0500

    Add filters

--- Course2FinalTask/Course2FinalTask/Code/FiltersViewController.swift
+++ Course2FinalTask/Course2FinalTask/Code/FiltersViewController.swift
@@ -17,7 +17,7 @@ class FiltersViewController: UIViewController {
     var delegate: FiltersDelegate?
     var targetImage: UIImage?
     private let collectionCellIdentifier = String(describing: FilterCollectionViewCell.self)
-    let filters = [CIFilter]()
+    let filters = ["CIPhotoEffectInstant", "CIPhotoEffectMono", "CIPhotoEffectNoir", "CIPhotoEffectProcess", "CIPhotoEffectTonal", "CIPhotoEffectTransfer", "CIPinchDistortion", "CIPinLightBlendMode", "CIPixellate", "CIPointillize","CIScreenBlendMode", "CISepiaTone", "CISoftLightBlendMode", "CISpotColor", "CISpotLight", "CIStraightenFilter"]
 
     lazy var filtersCollection: UICollectionView  = {
         let layout = UICollectionViewFlowLayout()
@@ -43,20 +43,36 @@ class FiltersViewController: UIViewController {
         filtersCollection.backgroundColor = .green
     }
     
-//    private func setupFilters() {
-//        let a = CIFilter(
-//    }
+    private func applyFilter(_ image: UIImage?, at item: Int) -> UIImage? {
+        guard let image = image else { return nil }
+        print("\(item)"+filters[item])
+        let filter = CIFilter(name: filters[item])
+        
+        // convert UIImage to CIImage and set as input
+        
+        let ciInput = CIImage(image: image)
+        filter?.setValue(ciInput, forKey: "inputImage")
+        
+        // get output CIImage, render as CGImage first to retain proper UIImage scale
+        
+        let ciContext = CIContext()
+        guard let ciOutput = filter?.outputImage else { print("ddd"); return nil }
+
+        let cgImage = ciContext.createCGImage(ciOutput, from: ciOutput.extent)
+        
+        return UIImage(cgImage: cgImage!)
+    }
 }
 
 extension FiltersViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 9
+        return filters.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = filtersCollection.dequeueReusableCell(withReuseIdentifier: collectionCellIdentifier,
                                                          for: indexPath as IndexPath) as! FilterCollectionViewCell
-        cell.imageView.image = targetImage
+        cell.imageView.image = applyFilter(targetImage, at: indexPath.item)
         return cell
     }
     
